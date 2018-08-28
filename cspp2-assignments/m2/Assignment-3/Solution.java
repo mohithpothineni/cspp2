2e6484a94ef757f5cea017ddda8fa965d98067c8
mohithpothineni <mohith.pothineni@msitprogram.net>
2018-08-28 19:32:25 +0530

    "check_style_score -> 1 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m2/Assignment-3/Solution.java
@@ -0,0 +1,41 @@
+
+
+import java.util.Scanner;
+/**
+*Solution for finding power by recursion.
+*/
+
+public final class Solution {
+    /**
+    *default constructor.
+    */
+    private Solution() {
+
+    }
+
+
+    /**
+    *Do not modify this main function.
+    @param args commandlineargs
+    */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int base = s.nextInt();
+        int exponent = s.nextInt();
+        long result = power(base, exponent);
+        System.out.println(result);
+    }
+
+    /**
+    *Need to write the power function and print the output.
+    @param a base
+    @param e exponant
+    @return result powered
+    */
+    public static long power(final long a, final int e) {
+        if (e == 0 || e == 1) {
+            return 1;
+        }
+        return a * power(a, e - 1);
+    }
+}

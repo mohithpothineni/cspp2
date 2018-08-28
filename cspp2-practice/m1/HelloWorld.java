4cbc0305a7236b199d0292f0cd4a9d34fe26207f
mohithpothineni <mohith.pothineni@msitprogram.net>
2018-08-28 07:47:53 +0530

    Helloworld

---
+++ cspp2-practice/m1/HelloWorld.java
@@ -0,0 +1,20 @@
+/**.
+ * @author Mohath
+ *
+ */
+final class HelloWorld {
+    /**.
+     * private constructor
+     */
+    private HelloWorld() {
+
+    }
+    /**.
+     * @param args are cli
+     */
+    public static void main(final String[] args) {
+        // TODO Auto-generated method stub
+    System.out.println("hello");
+    }
+
+}

ac16a8081d5879a580eb389e647af7d5516542a3
mohithpothineni <mohith.pothineni@msitprogram.net>
2018-08-28 18:10:55 +0530

    BiggerEqualSmaller

---
+++ cspp2-practice/m2/BiggerEqualSmaller.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+/**
+    *BiggerEqualSmaller class for assesing numbers.
+*/
+public final class BiggerEqualSmaller {
+
+    /**
+        *default constructor.
+    */
+    private BiggerEqualSmaller() {
+
+    }
+
+
+    /**
+        *Do not modify this main function.
+        @param args commandLineArgs
+    */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+        if(a > b)
+            System.out.println("Bigger");
+        else if(a == b)
+            System.out.println("Equal");
+        else if (a < c)
+            System.out.println("Smaller");
+        
+        
+    }
+
+}
